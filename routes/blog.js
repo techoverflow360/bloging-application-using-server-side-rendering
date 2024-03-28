7d0d4388f29e8b00bd415478c7d4b4a19b09742a
techoverflow360 <nayankumar200222@gmail.com>
2024-03-29 00:20:44 +0530

    blog details page, comment featues, first phase completed

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const Blog = require('../models/blog');
+const Comment = require('../models/comment');
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -34,6 +36,19 @@ const upload = multer({ storage: storage });
 
 const router = express.Router();
 
+router.get('/:id', async (req, res) => {
+    // whenever we have a reference, we expand it using populate 
+    const blog = await Blog.findById(req.params.id).populate('createdBy');
+    const comments = await Comment.find({blogId: req.params.id}).populate('createdBy');
+    console.log(comments);
+    // console.log(blog);
+    return res.render('blog', {
+        user: req.user,
+        blog: blog,
+        comments: comments,
+    });
+})
+
 router.get('/add-new', (req, res) => {
     res.render('addblog', { user: req.user });
 });
@@ -51,5 +66,18 @@ router.post('/', upload.single('coverImageURL') ,async (req, res) => {
     })
     return res.redirect(`/blog/${blog._id}`);
 })
+
+router.post('/comment/:blogId', async (req, res) => {
+    console.log(req.body);
+    const { content } = req.body;
+    const comment = await Comment.create({
+        content: content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
+
+
 
 module.exports = router;
